8223b0fc09bc19dd2bc774e0f56078b93531d3de
mcg <1478427231@qq.com>
2023-11-16 17:26:48 +0800

    马驰贵第一次提交代码

--- graduate-admin/src/main/java/weichun.java
+++ graduate-admin/src/main/java/weichun.java
@@ -2,5 +2,6 @@ public class weichun {
     public static void main(String[] args) {
         System.out.println("韦春第一次提交测试代码");
         System.out.println("雷神来啦");
+        System.out.println("小马哥来了");
     }
 }
